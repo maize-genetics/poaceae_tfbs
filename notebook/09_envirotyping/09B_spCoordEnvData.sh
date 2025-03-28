59b1daba51f1f7afeb4ea0b57019c15d95c1916c
ShengKaiHsu <46195776+ShengKaiHsu@users.noreply.github.com>
2025-03-28 11:55:39 -0400

    Update 09B_spCoordEnvData.sh

--- notebook/09_envirotyping/09B_spCoordEnvData.sh
+++ notebook/09_envirotyping/09B_spCoordEnvData.sh
@@ -3,6 +3,6 @@
 # run 12_metadataProcessing.ipynb
 # species name -> coordinate
 mkdir output/metadataFormalOut
-Rscript src/08_pulling_geo_data.R data/spNameMetadata_20240819.txt output/metadataFormalOut
+Rscript src/09_envirotying/01_pulling_geo_data.R data/spNameMetadata_20240819.txt output/metadataFormalOut
 
-#run 09_pulling_envData.r
+Rscript src/09_envirotying/02_pulling_envData.R
