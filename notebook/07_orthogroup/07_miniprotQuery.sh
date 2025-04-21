8d1e93c7098bc3c387ede139f503843a4dec49c8
Aimee Schulz <aschulzj@gmail.com>
2025-04-21 13:30:38 -0500

    Create 07_miniprotQuery.sh

---
+++ notebook/07_orthogroup/07_miniprotQuery.sh
@@ -0,0 +1,5 @@
+## Author: Aimee Schulz
+## Last updated: 04/21/2025
+## Generic MiniProt script parameters that was used at scale on SciNet. For SLURM scripts, contact Aimee Schulz at ajs692@cornell.edu
+
+miniprot --gff-only -t 30 -L 0 {path/to/shortReadAssembly.fa} Pv-672_v3.0_peptide.fa > shortReadAssembly.gff
