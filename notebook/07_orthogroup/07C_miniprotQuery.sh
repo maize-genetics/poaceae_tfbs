59e6090c96af4161a84d598e1e7f06af172dced4
Aimee Schulz <aschulzj@gmail.com>
2025-04-21 13:44:22 -0500

    Update 07C_miniprotQuery.sh

--- notebook/07_orthogroup/07C_miniprotQuery.sh
+++ notebook/07_orthogroup/07C_miniprotQuery.sh
@@ -1,5 +1,7 @@
 ## Author: Aimee Schulz
 ## Last updated: 04/21/2025
-## Generic MiniProt script parameters that was used at scale on SciNet. For SLURM scripts, contact Aimee Schulz at ajs692@cornell.edu
+## Generic MiniProt script parameters that was used at scale on SciNet. 
+## This script requires an assembly fasta file and the reference/orthogroup consensus sequence peptide file that you want to use for extracting sequence for each orthogroup. 
+## For SLURM scripts, contact Aimee Schulz at ajs692@cornell.edu
 
-miniprot --gff-only -t 30 -L 0 {path/to/shortReadAssembly.fa} Pv-672_v3.0_peptide.fa > shortReadAssembly.gff
+miniprot --gff-only -t 30 -L 0 {path/to/assembly.fa} {path/to/orthogroupSequencePeptides.fa} > assemblyOutput.gff
